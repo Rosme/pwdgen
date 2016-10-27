1377f0ad429d22ca5f103e122102a5745f730945
Jean-Sébastien Fauteux <jeanseb.fauteux@gmail.com>
2016-10-27 15:46:15 -0400

    Generate password

--- app.rb
+++ app.rb
@@ -1,25 +1,88 @@
 # coding: utf-8
 
-count = 16
+require 'sinatra'
+
+class Password
+    @@special = ['/', '\\', '&', '%', '$', '!', '(', ')', '@', '#', '?']
+    @@chars = ('a'..'z').to_a + ('0'..'9').to_a + ('A'..'Z').to_a
+    @@leet = ["h4ck", "5h3ll", "h4x0r", "pwn", "p0p"]
+    
+
+    def initialize(count, special, leet)
+        @count = count
+        @hasSpecial = special
+        @hasLeet = leet
+    end
+
+    def generate()
+        if @hasSpecial
+            @@special.each do|s|
+                @@chars << s
+            end
+        end
+        
+        if @hasLeet
+            @@leet.each do|s|
+                @@chars << s
+            end
+        end
+
+        i = 0
+        pwd = ""
+        begin
+            word = @@chars.sample()
+            pwd << word
+            i += word.length
+        end while i < @count
+
+        return pwd
+    end
+end
+
+count = 12
 
 if ARGV.length == 1
     count = ARGV[0].to_i
 end
 
-special = ['/', '\\', '&', '%', '$', '!', '(', ')', '@', '#', '?']
-chars = ('a'..'z').to_a + ('0'..'9').to_a + ('A'..'Z').to_a
-leet = ["h4ck", "5h3ll", "h4x0r", "pwn", "p0p"]
 
-special.each do|s|
-    chars << s
+
+def getPassword()
 end
 
-i = 0
+get '/' do
+    erb :index
+end
 
-password = ""
-begin
-    password << chars.sample()
-    i += 1
-end while i < count
+get '/pwd' do
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST'
 
-puts password
+    pass = Password.new(12, true, false)
+    pass.generate()
+end
+
+post '/pwd' do
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST'
+   
+    count = '12'
+    special = 'on'
+    leet = 'off'
+
+    if params.has_key?('count')
+        count = params['count']
+    end
+
+    if params.has_key?('special')
+        special = params['special']
+        puts special
+    end
+
+    if params.has_key?('leet')
+        leet = params['leet']
+    end
+
+    pass = Password.new(count.to_i, special == 'on'?true:false, leet == 'on'?true:false)
+    pass.generate()
+end
